a59675ef67a1c9d8c03c4b273113181cf62ee3e6
Murilo <murkuehne@gmail.com>
2025-11-14 20:55:29 -0300

    Fixed bug

--- components/charts/CategoryChart.tsx
+++ components/charts/CategoryChart.tsx
@@ -30,9 +30,11 @@ export const CategoryChart = ({ data }: CategoryChartProps) => {
             cx="50%"
             cy="50%"
             labelLine={false}
-            label={({ name, percent }) =>
-              `${name} ${(percent * 100).toFixed(0)}%`
-            }
+            label={(props) => {
+              const { name = '', percent = 0 } = props
+              const percentValue = percent ?? 0
+              return `${name} ${(percentValue * 100).toFixed(0)}%`
+            }}
             outerRadius={80}
             fill="#8884d8"
             dataKey="value"
